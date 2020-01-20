a52c4f4cb12ad97b2154095453d9b756e4d53919
David Boulad <daoud_boulad@hotmail.com>
2020-01-20 11:36:19 +0200

    travis

--- config/dev.js
+++ config/dev.js
@@ -2,6 +2,7 @@ module.exports = {
   googleClientID:
     '964808011168-29vqsooppd769hk90kjbjm5gld0glssb.apps.googleusercontent.com',
   googleClientSecret: 'KnH-rZC23z4fr2CN4ISK4srN',
-  mongoURI: 'mongodb://readonly:password@ds063124.mlab.com:63124/blog_everyone',
-  cookieKey: '123123123'
+  mongoURI: 'mongodb://test:test@cluster-1-shard-00-00-0cjhi.mongodb.net:27017,cluster-1-shard-00-01-0cjhi.mongodb.net:27017,cluster-1-shard-00-02-0cjhi.mongodb.net:27017/test?ssl=true&replicaSet=Cluster-1-shard-0&authSource=admin&retryWrites=true&w=majority',
+  cookieKey: '123123123',
+  redisUrl: 'redis://localhost:6379'
 };
